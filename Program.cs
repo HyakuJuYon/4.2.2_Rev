a6ffbe2a195c1ee85064922b7f1d7dd3f3349403
Hyaku Ju Yon <ichitora114@outlook.com>
2021-07-22 02:55:02 +0300

    While + Do While

--- Program.cs
+++ Program.cs
@@ -6,9 +6,67 @@ namespace _4._2._2_Rev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cycle Do");
+            
+        Console.WriteLine("Cycle While");
 
             int a = 0;
+
+            while (a < 1)
+
+            {
+                Console.WriteLine(a);
+
+                Console.WriteLine("Write down your favourite color in English using small letters >");
+
+                switch (Console.ReadLine())
+
+                {
+                    case "red":
+
+                        Console.BackgroundColor = ConsoleColor.Red;
+
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your fav color is red!");
+
+                        break;
+
+                    case "green":
+
+                        Console.BackgroundColor = ConsoleColor.Green;
+
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your fav color is green!");
+
+                        break;
+
+                    case "cyan":
+
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your fav color is cyan!");
+
+                        break;
+
+                    default:
+
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("You didn't pick up the color!");
+
+                        break;
+                } a++;
+
+        }
+
+        Console.WriteLine("Cycle Do While");
+
+            int b = 0;
 
             do
 
@@ -59,11 +117,9 @@ namespace _4._2._2_Rev
                         Console.WriteLine("You didn't pick up the color!");
 
                         break;
-                }
+                }  b++;
 
-                a++;
-
-            } while (a < 3);
+            } while (b < 3);
         }
     }
 }
