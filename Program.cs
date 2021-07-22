60919d73216b34fb550dbe081ce3ce46da26fc49
Hyaku Ju Yon <ichitora114@outlook.com>
2021-07-22 03:16:16 +0300

    Redirect

--- Program.cs
+++ Program.cs
@@ -11,115 +11,24 @@ namespace _4._2._2_Rev
 
             int a = 0;
 
-            while (a < -1)
+            while (true)
 
             {
                 Console.WriteLine(a);
 
                 Console.WriteLine("Write down your favourite color in English using small letters >");
 
-                switch (Console.ReadLine())
+                var text = Console.ReadLine();
 
-                {
-                    case "red":
+                if (text == "stop")
 
-                        Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Cycle ended");
 
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your fav color is red!");
-
-                        break;
-
-                    case "green":
-
-                        Console.BackgroundColor = ConsoleColor.Green;
-
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your fav color is green!");
-
-                        break;
-
-                    case "cyan":
-
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your fav color is cyan!");
-
-                        break;
-
-                    default:
-
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("You didn't pick up the color!");
-
-                        break;
-                } a++;
+                    break;
+            }
 
         }
-
-        Console.WriteLine("Cycle Do While");
-
-            int b = 0;
-
-            do
-
-            {
-                Console.WriteLine(a);
-
-                Console.WriteLine("Write down your favourite color in English using small letters >");
-
-                switch (Console.ReadLine())
-
-                {
-                    case "red":
-
-                        Console.BackgroundColor = ConsoleColor.Red;
-
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your fav color is red!");
-
-                        break;
-
-                    case "green":
-
-                        Console.BackgroundColor = ConsoleColor.Green;
-
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your fav color is green!");
-
-                        break;
-
-                    case "cyan":
-
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your fav color is cyan!");
-
-                        break;
-
-                    default:
-
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("You didn't pick up the color!");
-
-                        break;
-                }  b++;
-
-            } while (b < -1);
-        }
+       
     }
 }
+
